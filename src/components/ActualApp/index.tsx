1255e513f669dc86e889431ac1d416a2a36d66f0
rob3rtu <robertudrea2002@yahoo.ro>
2022-08-21 18:32:31 +0300

    a long time ago in a galaxy far, far away

--- src/components/ActualApp/index.tsx
+++ src/components/ActualApp/index.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { Box } from "@chakra-ui/react";
+import {
+  Animator,
+  Fade,
+  ScrollContainer,
+  ScrollPage,
+} from "react-scroll-motion";
+import { ALongTimeAgo } from "./Text Components/ALongTimeAgo";
 
 export const ActualApp = () => {
-  return <h1>Hello there Kenobi!</h1>;
+  return (
+    <Box bg="black">
+      <ScrollContainer>
+        <ScrollPage>
+          <Animator animation={Fade()}>
+            <ALongTimeAgo />
+          </Animator>
+        </ScrollPage>
+        <ScrollPage>
+          <Animator animation={Fade()}>
+            <h1>Hello there</h1>
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer>
+    </Box>
+  );
 };
