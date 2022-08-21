1dd6e63d1e5026e34a794063c3df50b01547d4f5
rob3rtu <robertudrea2002@yahoo.ro>
2022-08-21 16:57:31 +0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { BasicAnimations } from "./components/BasicAnimations";
+import { ActualApp } from "./components/ActualApp";
+import { BasicAnimations } from "./components/Playground";
 
 export const App = () => {
-  return <BasicAnimations />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<ActualApp />} />
+        <Route path="playground" element={<BasicAnimations />} />
+        <Route path="*" element={<h1>404 Page not found!</h1>} />
+      </Routes>
+    </>
+  );
 };
---
+++ src/components/ActualApp/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ActualApp = () => {
+  return <h1>Hello there Kenobi!</h1>;
+};
